73979d7cddb4fbb2987580e690a517d30365eff9
HugoLaurent <lauhugopro@gmail.com>
2023-01-20 16:17:26 +0100

    js en cours

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+var submit = document.getElementsByClassName('submit');
+
+submit.addEventListener('click', show());
+
+function show () {
+    submit.style.display = 'none';
+}
+
+console.log(submit)
