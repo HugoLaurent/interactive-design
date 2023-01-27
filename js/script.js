a14e2b02821f3505bb26ecaaf18d74f35e548ac4
HugoLaurent <lauhugopro@gmail.com>
2023-01-27 19:31:40 +0100

    Finish

--- js/script.js
+++ js/script.js
@@ -1,34 +1,16 @@
-const choosenNote = document.querySelectorAll(".buttonNote");
-let x;
+document.querySelector("#submit-button").addEventListener("click", (e) => {
+    document.querySelector("#landing-box").classList.add("disapear");
+    document.querySelector("#result-box").classList.add("appear");
+ })
 
-for (let i = 0; i < choosenNote.length; i++) {
-    choosenNote[i].addEventListener("mouseover", function(event) {
-        setTimeout(() => {
-            event.target.style.backgroundColor = 'hsl(25, 97%, 53%)';
-        if (event.target.previousElementSibling){
-            event.target.previousElementSibling.style.backgroundColor= "hsl(216, 12%, 54%, .8";
-        }
-        if (event.target.nextElementSibling) {
-        event.target.nextElementSibling.style.backgroundColor= "hsl(216, 12%, 54%, .8)";
-        }
-        }, 100);
-    });
+const rates = document.querySelectorAll(".button-value")
+for (let i = 0; i < rates.length; i++) {
+        rates[i].addEventListener("click", function(event) {
+            let rate = rates[i].value
+            document.querySelector(".final-rate").textContent = `You selected ${rate} out of 5`
+        })
+        
 }
-
-for (let i = 0; i < choosenNote.length; i++) {
-    choosenNote[i].addEventListener("mouseout", function(event) { 
-        setTimeout(() => {
-              event.target.style.backgroundColor = 'hsl(216, 12%, 54%, .2)';
-        if (event.target.previousElementSibling){
-            event.target.previousElementSibling.style.backgroundColor= "hsl(216, 12%, 54%, .2)";
-        }
-        if (event.target.nextElementSibling) {
-        event.target.nextElementSibling.style.backgroundColor= "hsl(216, 12%, 54%, .2)";
-        } 
-        }, 100)
-      
-    });
-
     
 
 
@@ -36,42 +18,4 @@ for (let i = 0; i < choosenNote.length; i++) {
 
 
 
-
-
-}
-
-console.log(x);
-    // if (x === 1) {
-    //                 document.querySelector("#btn1").style.backgroundColor = 'hsl(25, 97%, 53%)';
-    //                 document.querySelector("#btn2").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //             } else if (x === 2) {
-    //                 document.querySelector("#btn1").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //                 document.querySelector("#btn2").style.backgroundColor = 'hsl(25, 97%, 53%)';
-    //                 document.querySelector("#btn3").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //             } else if (x === 3) {
-    //                 document.querySelector("#btn2").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //                 document.querySelector("#btn3").style.backgroundColor = 'hsl(25, 97%, 53%)';
-    //                 document.querySelector("#btn4").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //             } else if (x === 4) {
-    //                 document.querySelector("#btn3").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //                 document.querySelector("#btn4").style.backgroundColor = 'hsl(25, 97%, 53%)';
-    //                 document.querySelector("#btn5").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //             } else if (x === 5) {
-    //                 document.querySelector("#btn5").style.backgroundColor = 'hsl(25, 97%, 53%)';
-    //                 document.querySelector("#btn4").style.backgroundColor = 'hsl(216, 12%, 54%, .8)';
-    //             } else {
-    //                 document.querySelector("#btn1").style.backgroundColor = '';
-    //                 document.querySelector("#btn2").style.backgroundColor = '';
-    //                 document.querySelector("#btn3").style.backgroundColor = '';
-    //                 document.querySelector("#btn4").style.backgroundColor = '';
-    //                 document.querySelector("#btn5").style.backgroundColor = '';
-    //             }
-
-
-
-// function changeColorOff() {
-//     bouton2.style.backgroundColor = 'hsl(216, 12%, 54%, .2)';
-//     bouton3.style.backgroundColor = 'hsl(216, 12%, 54%, .2)';
-//     bouton4.style.backgroundColor = 'hsl(216, 12%, 54%, .2)';
-// };
 
